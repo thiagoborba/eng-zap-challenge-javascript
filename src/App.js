37677a86b2d24b5946788f234c389efcf5c9bd35
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-05-30 20:52:26 -0300

    feat: adiciona router ao app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import { Routes } from './routes'
 
 function App() {
-  return (
-    <div className="App">
-      Home
-    </div>
-  );
+  return (<Routes />);
 }
 
 export default App;
