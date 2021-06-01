4cf0c4438cec1020f8a237a26a74aeed3638ff33
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-05-31 21:02:46 -0300

    feat: Cria lógica da regra de negócio na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { getProperties } from '../../api'
+import React, { useEffect, useMemo, useState } from 'react'
+import { getProperties } from '../../api';
+import { Loader } from '../../Components/Loader'
+import { Title } from '../../Components/Title';
+import { Spacing } from '../../Components/Spacing';
+
+const BUSINESS_TYPE = {
+  SALE: "SALE",
+  RENTAL: 'RENTAL'
+}
+
+const ZAP_MINIMAL_RENTAL_PRICE = 3500
+const ZAP_MINIMAL_SELLING_PRICE = 600000
+
+const VIVA_REAL_MAXIMUM_RENTAL_PRICE  = 4000
+const VIVA_REAL_MAXIMUM_SELLING_PRICE = 700000
 
 export const Home = () => {
-  const [{ loading }, setState] = useState({
-    loading: false
+  const [{ loading, zapProprieties, vivaRealProprieties, proprieties }, setState] = useState({
+    loading: false,
+    proprieties: [],
+    zapProprieties: [],
+    vivaRealProprieties: []
   })
+
+  const memoZapProprieties = useMemo(() => console.log(zapProprieties), [zapProprieties])
+  const memoVivaRealProprieties = useMemo(() => console.log(vivaRealProprieties), [vivaRealProprieties])
 
   useEffect(() => {
     fetchProprieties();
   }, []);
 
+  useEffect(() => {
+    setProprietiesByAgency(proprieties)
+  }, [proprieties])
+
   async function fetchProprieties() {
     try {
       setState((prevState) => ({ ...prevState, loading: true }));
       const proprieties = await getProperties();
-      console.log(proprieties)
+      setState(prevState => ({ ...prevState, proprieties }))
     } catch (error) {
       console.error(error)
     } finally {
@@ -22,9 +46,42 @@ export const Home = () => {
     }
   }
 
+  async function setProprietiesByAgency(props) {
+    const zapProprieties = []
+    const vivaRealProprieties = []
+
+    props.forEach((property) => {
+      const { pricingInfos: { businessType, rentalTotalPrice, price } } =  property
+
+      const intRentalTotalPrice = parseInt(rentalTotalPrice)
+      const intPrice= parseInt(price)
+
+      const isEligibleForZap = 
+      ((businessType === BUSINESS_TYPE.RENTAL) && (intRentalTotalPrice >= ZAP_MINIMAL_RENTAL_PRICE)) ||
+      ((businessType === BUSINESS_TYPE.SALE) && (intPrice >= ZAP_MINIMAL_SELLING_PRICE))
+
+      const isEligibleForViva = 
+      ((businessType === BUSINESS_TYPE.RENTAL) && (intRentalTotalPrice <= VIVA_REAL_MAXIMUM_RENTAL_PRICE)) ||
+      ((businessType === BUSINESS_TYPE.SALE) && (intPrice <= VIVA_REAL_MAXIMUM_SELLING_PRICE))
+
+      if (isEligibleForZap) zapProprieties.push(property)
+      if (isEligibleForViva) vivaRealProprieties.push(property)
+    })
+
+    setState(prevState => ({ ...prevState, zapProprieties, vivaRealProprieties }))
+  }
+
+  function renderCards () {
+    
+  }
+
   return (
     <div>
-      Home
+      <Loader show={loading}/>
+      <Title variant='h3' align='center'>
+        Grupo Zap
+      </Title>
+      <Spacing height='40px'/>
     </div>
   )
 }
