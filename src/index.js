e2159fe1108f5f0fe741828c2c8a14184f93b622
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-01 03:14:08 -0300

    feat: Adiciona CssBaseline

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css'
+import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
