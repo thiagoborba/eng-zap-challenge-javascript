43c6ec5191999d776bf24e59dfcf71436a65271f
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-02 06:22:58 -0300

    feat: salva endpoint em variavel para utilizar em teste

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+export const PROPRIETIES_ENDPOINT = 'http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json'
+
 export async function getProperties () {
-  return await fetch('http://grupozap-code-challenge.s3-website-us-east-1.amazonaws.com/sources/source-1.json').then(resp => resp.json())
+  return await fetch(PROPRIETIES_ENDPOINT).then(resp => resp.json())
 }
