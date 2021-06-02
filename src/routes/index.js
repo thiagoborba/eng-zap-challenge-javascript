4e76ffcbad9b90e51d9809d05ee45bd4fd2da3e0
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-02 06:25:38 -0300

    feat: adiciona pagina details no router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import { Details } from '../pages/Details'
+import { PAGE } from '../constants'
 
 export const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path='/' component={Home} />
+      <Route exact path={PAGE.HOME()} component={Home} />
+      <Route exact path={PAGE.DETAILS()} component={Details} />
     </Switch>
   </Router>
 )
