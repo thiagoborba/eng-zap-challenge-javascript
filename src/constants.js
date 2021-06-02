dc66316033b863e4a8496535a5783ce9ee4826b8
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-02 02:00:17 -0300

    feat:  Transfere constantes para arquivo separado

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const BUSINESS_TYPE = {
+  SALE: "SALE",
+  RENTAL: 'RENTAL'
+}
+
+export const ZAP_MINIMAL_RENTAL_PRICE = 3500
+export const ZAP_MINIMAL_SELLING_PRICE = 600000
+
+export const VIVA_REAL_MAXIMUM_RENTAL_PRICE  = 4000
+export const VIVA_REAL_MAXIMUM_SELLING_PRICE = 700000
+
+export const VIEW = {
+  ZAP: 'ZAP',
+  VIVA: 'VIVA'
+}
+
+export const PAGE_SIZE = 20
