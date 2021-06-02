fc25ff30abd82c451ebf873f7b55b310e4258555
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-02 02:26:00 -0300

    feat: Cria componente Grid

---
+++ src/Components/Grid/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Grid as MateiralGrid, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+export const Grid = ({ children, ...props }) => {
+  const classes = useStyles()
+  return (
+    <MateiralGrid
+      { ...props }
+      className={classes.root}
+      container
+      alignItems='center'
+      justify='center'
+    >
+      {children}
+    </MateiralGrid>
+  )
+}
+
+export default Grid
