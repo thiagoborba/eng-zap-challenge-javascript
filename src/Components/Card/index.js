3c269a891c6675473168025fd7dcaa84ed28a725
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-02 02:25:41 -0300

    feat: Cria compoenente Card

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,42 +1,104 @@
-import React  from 'react'
+import React from 'react'
+import { Box, Card as MaterialCard, CardContent } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card as CompCard } from '@material-ui/core'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
+import Carousel from 'react-material-ui-carousel'
+import { Spacing, Title } from '../../Components'
+import { BUSINESS_TYPE } from '../../constants'
 
 const useStyles = makeStyles({
   card: {
-    minWidth: 275,
+    '&:hover': {
+      cursor: 'pointer',
+      'box-shadow': '5px 5px 5px -2px rgba(0,0,0,0.34)',
+      '-webkit-box-shadow': '5px 5px 5px -2px rgba(0,0,0,0.34)',
+      '-moz-box-shadow': '5px 5px 5px -2px rgba(0,0,0,0.34)',
+    },
     margin: 32,
+    minWidth: 300,
+    maxWidth: 350
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  box: {
+    overflow: 'hidden',
+    height: '20vh',
   },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+  img: {
+    objectFit: 'cover',
+    width: '100%',
+    height: '100%',
+  }
 })
 
-export const Card = props =>  {
+function getBusinessLabel(businessType) {
+  switch (businessType) {
+    case BUSINESS_TYPE.SALE:
+      return 'venda'
+    case BUSINESS_TYPE.RENTAL:
+      return 'aluguel'
+    default:
+      break;
+  }
+}
+
+function getPriceLabel (pricingInfos) {
+  const { businessType, price, rentalTotalPrice } = pricingInfos
+  switch (businessType) {
+    case BUSINESS_TYPE.SALE:
+      return `R$ ${price}`
+    case BUSINESS_TYPE.RENTAL:
+      return `R$ ${rentalTotalPrice}/Mês`
+    default:
+      break;
+  }
+}
+
+export const Card = ({ property, onClick }) => {
   const classes = useStyles()
-  const { property } = props
+
+  const { address: { city, neighborhood } } = property
 
   return (
-    <CompCard className={classes.card}>
+    <MaterialCard onClick={onClick} className={classes.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          { property.id }
-        </Typography>
-        <img   alt='asdasd' src={property.images[0]}/>
+        <Title variant='h5'>
+          Imovel para {getBusinessLabel(property.pricingInfos.businessType)}
+        </Title>
+        <Spacing height='8px' />
+        <Title paragraph>
+        { city } - { neighborhood }s
+        </Title>        
+        <Spacing height='8px' />
+        <Carousel autoPlay={false}>
+          {property.images.map(img => (
+            <Box className={classes.box} key={property.id} >
+              <img alt='apartment' className={classes.img} src={img} />
+            </Box>
+          ))}
+        </Carousel>
+        <Spacing height='16px' />
+        <Title paragraph>
+          { getPriceLabel(property.pricingInfos) }
+        </Title>
+        <Title paragraph>
+          Quartos: { property.bedrooms }
+        </Title>
+        <Spacing height='8px' />
+        <Title paragraph>
+          Banheios: { property.bathrooms }
+        </Title>
+        <Spacing height='8px' />
+        <Title paragraph>
+          Banheios: { property.bathrooms }
+        </Title>
+        <Spacing height='8px' />
+        <Title paragraph>
+          Área útil: { property.usableAreas }/M²
+        </Title>
+        <Spacing height='8px' />
+        <Title paragraph>
+          V. Garagem: { property.parkingSpaces }
+        </Title>
       </CardContent>
-    </CompCard>
+    </MaterialCard>
   )
 }
 
