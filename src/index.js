4b894deec54620759369afab7548847d3d5882dc
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-03 02:31:42 -0300

    feat: remove CssBaseline

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CssBaseline } from '@material-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
