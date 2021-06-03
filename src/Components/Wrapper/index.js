294cbbb15c3bab97bcce609d98d95bca32c950ba
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-03 02:34:20 -0300

    feat: Cria componente wrapper

---
+++ src/Components/Wrapper/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Box, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      flex: 1,
+      marginRight: 24,
+  
+      '&:last-child': {
+        marginRight: 0,
+      }
+    },
+  
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+  
+      '& > *': {
+        flex: 'auto',
+  
+      '&:not(:last-child)': {
+          margin: '0 0 16px 0',
+        }
+      }
+    }
+  }
+}))
+
+export const Wrapper = (props) => {
+  const classes = useStyles()
+  console.log(classes)
+  return (
+    <Box 
+      { ...props }
+      className={classes.root}
+    />
+  )
+}
+
+export default Wrapper
