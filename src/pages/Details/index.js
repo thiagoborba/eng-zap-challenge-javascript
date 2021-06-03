1cb02ce43b359067efadb1c5029531175fcec090
thiago_borba <thiago_borba@terceiros.sicredi.com.br>
2021-06-03 02:33:37 -0300

    feat: Cria tela de detalhes

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,91 @@
-import React from 'react'
+import React from 'react';
+import { TextField, Spacing, Title, Button } from '../../Components';
+import {
+  BUSINESS_TYPE,
+  PAGE,
+} from '../../constants';
+import { useContext } from '../../Store'
+import { Container } from  '@material-ui/core'
+import { getBusinessLabel } from '../../utils'
 
-export const Details = () => (
-  <div>details</div>
-)
+export const Details = ({ history }) => {
+  const { resetSelectedProperty, state }  = useContext()
 
-export default Details 
+  const { selectedProperty } = state
+  const {
+    id,
+    usableAreas,
+    bedrooms,
+    bathrooms,
+    address: { city, neighborhood },
+    pricingInfos: { businessType },
+    parkingSpaces,
+  } = selectedProperty
+
+  return (
+    <Container style={{ maxWidth: 600 }}>
+      <Title variant='h5' align='center'>
+        Detalhes do Imovel
+      </Title>
+      <Spacing height='40px'/>
+      <Title paragraph align='center'>
+        Confira abaixo as fotos e as informações detalhadas do imóvel selecionado 
+      </Title>
+      <Spacing height='40px'/>
+      <TextField
+        label={'Código de identificação'}
+        value={id}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Tipo'}
+        value={`imovel para ${getBusinessLabel(businessType)}`}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Endereço'}
+        value={`${ neighborhood } - ${city}`}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Quartos'}
+        value={bedrooms}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Banheios'}
+        value={bathrooms}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Área útil'}
+        value={`${usableAreas}/M²`}
+        disabled
+      />
+      <Spacing height='16px'/>
+      <TextField
+        label={'Vagas de garagem'}
+        value={parkingSpaces}
+        disabled
+      />
+      <Spacing height='40px'/>
+      <Button
+        data-testid='back'
+        children='Voltar'
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          resetSelectedProperty()
+          history.goBack()
+        }}
+      />
+    </Container>
+  )
+}
+
+export default Details
